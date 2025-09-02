11273ec2214bc621900225a3ef684e81cfd8cc76
sofia Salles Santos <133766346+guapoduo@users.noreply.github.com>
2025-09-02 11:02:17 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+// Variáveis
+const idade = 16; 
+let nome = "";    
+
+// Função que pega o valor do input
+function mostrarNome() {
+  nome = document.getElementById("nome").value;
+  console.log("Nome digitado:", nome); 
+  console.log("Idade fixa:", idade);
+}
+Co
